b75c7b3373a4d658e190ee1138c66f1091c97a9d
GT301 <taoguo301@gmail.com>
2021-02-02 12:05:41 -0600

    Added CSV file to seeds.rb, and fixed ProductionCompany validation uniqueness

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :production_company
-  validates :title, :year, :duration, :average_vote, :description, presence: true
-  validates :title, uniqueness: true
+  validates :titile, :year, :duration, :average_vote, :description, presence: true
+  validates :titile, uniqueness: true
   validates :year, :duration, numericality: { only_integer: true }
   validates :average_vote, numericality: true
 end
--- db/migrate/20210202172553_create_movies.rb
+++ db/migrate/20210202172553_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
-      t.string :title
+      t.string :titile
       t.integer :year
       t.integer :duration
       t.text :description
